f61896c504b9dbbb947a227be309bdca55e8d262
Dmitry <d.vologzhaninov@gmail.com>
2025-02-04 19:23:09 -0800

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://dvologzhanin-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://dvologzhanin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 
+                 'https://dvologzhanin-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+            'https://dvologzhanin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://dvologzhanin-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://dvologzhanin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+            'https://dvologzhanin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -94,21 +95,17 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    {'NAME': 
+     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+    {'NAME':
+     'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+    {'NAME':
+     'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+    {'NAME':
+     'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -145,4 +142,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
