7a13d8a2eef82af751f0b683bdc87bdcd8634dea
Dmitry <d.vologzhaninov@gmail.com>
2025-02-04 20:29:00 -0800

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://dvologzhanin-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://dvologzhanin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://dvologzhanin-8000.theianext-1-labs-prod-misc-tools-us-east-0 \ 
+    .proxy.cognitiveclass.ai',
+    'https://dvologzhanin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01 \ 
+    .proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://dvologzhanin-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://dvologzhanin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://dvologzhanin-8000.theianext-1-labs-prod-misc-tools-us-east-0 \ 
+    .proxy.cognitiveclass.ai',
+    'https://dvologzhanin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01 \ 
+    .proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -98,13 +102,13 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME':
-     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     {'NAME':
-     'django.contrib.auth.password_validation.MinimumLengthValidator',},
+     'django.contrib.auth.password_validation.MinimumLengthValidator', },
     {'NAME':
-     'django.contrib.auth.password_validation.CommonPasswordValidator',},
+     'django.contrib.auth.password_validation.CommonPasswordValidator', },
     {'NAME':
-     'django.contrib.auth.password_validation.NumericPasswordValidator',},
+     'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
 
 
